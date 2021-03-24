d195274e8943da2d8bca573b9a279a7aad8c9d60
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2021-03-24 13:49:43 +0100

    before proposal

--- pharaoh/pyramid/admin.py
+++ pharaoh/pyramid/admin.py
@@ -8,8 +8,16 @@ from .utils import calculate_payments_for_agents
 
 
 class AgentAdmin(admin.ModelAdmin):
-    list_display = ("name", "parent", "points")
+    list_display = ("name", "parent", "points", "level")
     search_fields = ("name", )
+
+    def level(self, obj):
+        if obj and obj.points:
+            return Level.objects.filter(points_to__gt=obj.points).order_by("level").first()
+        return "-"
+
+    level.short_description = _("Level")
+    level.admin_order_field = "points"
 
     # def save_form(self, request, form, change):
     #     super().save_form(request, form, change)
@@ -18,8 +26,8 @@ admin.site.register(Agent, AgentAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "coefficient", "adds_to_points", "adds_to_payment",)
-    list_filter = ("adds_to_points", "adds_to_payment",)
+    list_display = ("name", "coefficient", "points_type",)
+    list_filter = ("points_type",)
 
 admin.site.register(Product, ProductAdmin)
 
@@ -37,9 +45,15 @@ admin.site.register(Transaction, TransactionAdmin)
 
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ("agent", "amount", "created",)
+    list_display = ("agent", "for_month", "production_points", "reward_points", "fiscal_points", "career_points", "compensation")
 
     change_list_template = "admin/pyramid/payment/change_list.html"
+
+    def for_month(self, obj):
+        return obj.created.strftime("%m.%Y")
+
+    for_month.admin_order_field = 'created'
+    for_month.short_description = 'Month'
 
     def get_urls(self):
         urls = super().get_urls()
--- pharaoh/pyramid/models.py
+++ pharaoh/pyramid/models.py
@@ -47,12 +47,26 @@ class Level(models.Model):
 
 
 class Product(models.Model):
+    POINT_CHOICES = (
+        ("production", "Production points"),
+        ("reward", "Reward points"),
+        ("fiscal", "Fiscal points"),
+    )
+
     name = models.CharField(_("Name"), blank=True, null=True, max_length=200)
 
     coefficient = models.FloatField(_("Points coefficient"), blank=True, null=True)
 
     adds_to_points = models.BooleanField(_("Adds to points"), default=True)
     adds_to_payment = models.BooleanField(_("Adds to payment"), default=True)
+
+    points_type = models.CharField(_("Points type"),
+        max_length=50,
+        null=False,
+        blank=False,
+        choices=POINT_CHOICES,
+        default="production",
+    )
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
@@ -99,8 +113,19 @@ class Payment(models.Model):
         verbose_name=_("Agent"),
         on_delete=models.SET_NULL,
     )
-    amount = models.FloatField(_("Amount"), null=False)
+    amount = models.FloatField(_("Amount"), null=True, blank=True)
     month = models.DateTimeField(default=datetime.now, blank=True)
+
+    production_points =  models.FloatField(_("Production points"), null=True, blank=True)
+    reward_points = models.FloatField(_("Reward points"), null=True, blank=True)
+    fiscal_points = models.FloatField(_("Fiscal points"), null=True, blank=True)
+
+    career_points = models.FloatField(_("Total career points"), null=True, blank=True)
+    compensation= models.FloatField(_("Compensation"), null=True, blank=True)
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        verbose_name = _("Calculation")
+        verbose_name_plural = _("Calculations")
